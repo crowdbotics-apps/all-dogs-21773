ba1324d954bd8bd4110b6c0239caa209e75fb63d
Crowdbotics <team@crowdbotics.com>
2020-10-21 01:55:36 +0000

    add package blueprint-react-native-sign-in-5-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn51152193Navigator from '../features/SignIn51152193/navigator';
 import BlankScreen0152192Navigator from '../features/BlankScreen0152192/navigator';
 
 /**
@@ -14,6 +15,7 @@ import BlankScreen0152192Navigator from '../features/BlankScreen0152192/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn51152193: { screen: SignIn51152193Navigator },
 BlankScreen0152192: { screen: BlankScreen0152192Navigator },
 
     /** new navigators can be added here */
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn51152193Reducer from '../features/SignIn51152193/redux/reducers'
 
 export const combinedReducers = combineReducers({
   blank: (state, action) => {
@@ -15,5 +16,6 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn51152193: SignIn51152193Reducer,
 
 });
